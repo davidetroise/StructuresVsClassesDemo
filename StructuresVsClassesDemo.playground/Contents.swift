0136e2b7ec259da65b1a470a13c0c062c3f3b53d
Davide Troise <davide@troise.net>
2022-11-08 00:11:49 +0100

    Complete code

--- StructuresVsClassesDemo.playground/Contents.swift
+++ StructuresVsClassesDemo.playground/Contents.swift
@@ -1,3 +1,77 @@
-import UIKit
+struct Vehicle {
+    let numberOfWheels: Int
+    var isEngineOn: Bool
+    
+    init(numberOfWheels: Int, isEngineOn: Bool = false) {
+        self.numberOfWheels = numberOfWheels
+        self.isEngineOn = isEngineOn
+    }
+    
+    func printEngineStatus() {
+        if isEngineOn {
+            print("Engine is ON")
+        } else {
+            print("Engine is OFF")
+        }
+    }
+    
+    mutating func startEngine() {
+        if isEngineOn == false {
+            isEngineOn = true
+        }
+    }
+    
+    mutating func stopEngine() {
+        if isEngineOn == true {
+            isEngineOn = false
+        }
+    }
+}
 
-var greeting = "Hello, playground"
+var car = Vehicle(numberOfWheels: 4)
+let anotherCar = car
+
+car.printEngineStatus()
+car.startEngine()
+car.printEngineStatus()
+
+anotherCar.printEngineStatus()
+
+class Animal {
+    var commonName: String
+    var isAdult: Bool
+    
+    init(commonName: String, isAdult: Bool = false) {
+        self.commonName = commonName
+        self.isAdult = isAdult
+    }
+    
+    func printLifeStatus() {
+        if isAdult {
+            print("\(commonName) is an ADULT")
+        } else {
+            print("\(commonName) is a PUPPY")
+        }
+    }
+    
+    func growUp() {
+        if isAdult == false {
+            isAdult = true
+        }
+    }
+}
+
+class Cat: Animal {
+    init() {
+        super.init(commonName: "Cat")
+    }
+}
+
+let cat = Cat()
+let anotherCat = cat
+
+cat.printLifeStatus()
+cat.growUp()
+cat.printLifeStatus()
+
+anotherCat.printLifeStatus()
